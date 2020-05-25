bacca3dfe9063e8d5a76bbbec6a8f00b2aab887b
Núria Silla <nuriasilla@Nuria-Silla-MacBook-Pro.local>
2020-05-25 13:46:19 +0200

    [RED] should return false if number is composed

--- main.test.ts
+++ main.test.ts
@@ -56,6 +56,19 @@ test("should return true if number is prime", () => {
   expect(result).toBe(expected);
 });
 
+test("should return false if number is composed", () => {
+  // Arrange
+  let result;
+  const expected = false;
+  let customNumber = new CustomNumber();
+
+  // Act
+  result = customNumber.isPrime('8');
+
+  // Assert
+  expect(result).toBe(expected);
+});
+
 /*
 zero
 one
