dce594ab74ecdaa710a1962a2e592d0052a4f871
javimsevilla <javier.molina.sevilla@gmail.com>
2020-05-25 10:36:18 +0200

    [RED] should throw an error if it's not a number

--- main.test.ts
+++ main.test.ts
@@ -1,3 +1,18 @@
+import { CustomNumber } from './custom-number';
+
 test('Dummy test', () => {
   expect(true).toBe(true);
+});
+
+test("should throw an error if it's not a number", () => {
+  // Arrange
+  let result = '';
+  const expected = 'Input is not a number';
+  let customNumber = new CustomNumber();
+
+  // Act
+  result = customNumber.isPrime('a');
+
+  // Assert
+  expect(result).toThrowError(expected);
 });
