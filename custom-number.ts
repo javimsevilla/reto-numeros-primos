8e23a1ecbf41ac2c83055531c5c8660b298e57de
TSBSOLUCIONES\rvelez <rvelez@mysphera.com>
2020-05-25 12:06:29 +0200

    [BLUE] Extract constant

--- custom-number.ts
+++ custom-number.ts
@@ -1,6 +1,7 @@
 export class CustomNumber {
   private readonly NOT_A_NUMBER_MESSAGE = 'Input is not a number';
   private readonly NOT_A_NATURAL_NUMBER_MESSAGE = 'Input is not a natural number';
+  private readonly EMPTY_VALUE = '';
 
   isPrime(value: string): string {
     if (Number.isNaN(parseInt(value))) {
@@ -11,6 +12,6 @@ export class CustomNumber {
       throw new Error(this.NOT_A_NATURAL_NUMBER_MESSAGE);
     }
 
-    return '';
+    return this.EMPTY_VALUE;
   }
 }
